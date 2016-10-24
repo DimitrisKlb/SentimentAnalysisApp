7e5dc160e27ebcf6c8309e2804e7aafb9f595c63
Dimitris <kaldim11@gmail.com>
2016-10-24 23:25:28 +0300

    Initial commit

---
+++ SentimentAnalysisApp/App_Start/WebApiConfig.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Http;
+
+namespace SentimentAnalysisApp
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{searchKeyword}",
+                defaults: new { searchKeyword = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ SentimentAnalysisApp/Controllers/ServiceController.cs
@@ -0,0 +1,42 @@
+﻿using System.Net;
+using System.Web.Http;
+using Swashbuckle.Swagger.Annotations;
+
+namespace SentimentAnalysisApp.Controllers
+{
+    public class ServiceController : ApiController
+    {
+        static string defaultSearchKeywork = "coca cola";
+         
+        // GET api/values
+        [SwaggerOperation("GetAll")]
+        public string Get()
+        {
+            return getTweets(defaultSearchKeywork);
+        }
+
+        // GET api/values/searchKeywork
+        [SwaggerOperation("GetById")]
+        [SwaggerResponse(HttpStatusCode.OK)]
+        [SwaggerResponse(HttpStatusCode.NotFound)]
+        public string Get(string searchKeyword)
+        {
+            return getTweets(searchKeyword);
+        }
+
+        // POST api/values
+        [SwaggerOperation("Create")]
+        [SwaggerResponse(HttpStatusCode.Created)]
+        public void Post([FromBody]string value)
+        {
+            defaultSearchKeywork = value;
+        }
+
+        // Naive mine from Twitter, tweets thats contain searchKeyword
+        private static string getTweets(string searchKeyword)
+        {
+            return searchKeyword + " in text.";
+        }
+
+    }
+}
