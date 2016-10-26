ba98232e198c0ba0a6158d477e4028bf1a166e3a
Dimitris <kaldim11@gmail.com>
2016-10-26 15:34:59 +0300

    Add basic Twitter miner

--- SentimentAnalysisApp/Controllers/ServiceController.cs
+++ SentimentAnalysisApp/Controllers/ServiceController.cs
@@ -2,21 +2,26 @@
 using System.Web.Http;
 using Swashbuckle.Swagger.Annotations;
 
+using Tweetinvi;
+using Tweetinvi.Parameters;
+using Tweetinvi.Models;
+using System.Web.Configuration;
+
 namespace SentimentAnalysisApp.Controllers
 {
     public class ServiceController : ApiController
     {
-        static string defaultSearchKeywork = "coca cola";
+        static string defaultSearchKeywork = "coca-cola";
          
-        // GET api/values
-        [SwaggerOperation("GetAll")]
+    // GET api/values
+        [SwaggerOperation("GetDefaultKeyword")]
         public string Get()
         {
             return getTweets(defaultSearchKeywork);
         }
 
-        // GET api/values/searchKeywork
-        [SwaggerOperation("GetById")]
+    // GET api/values/searchKeywork
+        [SwaggerOperation("GetByKeyword")]
         [SwaggerResponse(HttpStatusCode.OK)]
         [SwaggerResponse(HttpStatusCode.NotFound)]
         public string Get(string searchKeyword)
@@ -24,7 +29,7 @@ namespace SentimentAnalysisApp.Controllers
             return getTweets(searchKeyword);
         }
 
-        // POST api/values
+    // POST api/values
         [SwaggerOperation("Create")]
         [SwaggerResponse(HttpStatusCode.Created)]
         public void Post([FromBody]string value)
@@ -32,9 +37,27 @@ namespace SentimentAnalysisApp.Controllers
             defaultSearchKeywork = value;
         }
 
-        // Naive mine from Twitter, tweets thats contain searchKeyword
+    // Naive miner from Twitter, to get tweets that contain searchKeyword
         private static string getTweets(string searchKeyword)
         {
+            string twitterConsumerKey, twitterConsumerSecret, twitterAccessToken, twitterAccessTokenSecret;
+            twitterConsumerKey = WebConfigurationManager.AppSettings["twitterConsumerKey"];
+            twitterConsumerSecret = WebConfigurationManager.AppSettings["twitterConsumerSecret"];
+            twitterAccessToken = WebConfigurationManager.AppSettings["twitterAccessToken"];
+            twitterAccessTokenSecret = WebConfigurationManager.AppSettings["twitterAccessTokenSecret"];
+
+            // Set up your credentials
+            Auth.SetUserCredentials(twitterConsumerKey, twitterConsumerSecret, twitterAccessToken, twitterAccessTokenSecret);
+
+            // Get relevant tweets
+            var searchParameters = new SearchTweetsParameters(searchKeyword)
+            {
+                Lang = LanguageFilter.English,
+                SearchType = SearchResultType.Mixed
+            };
+            var theTweets1 = Search.SearchTweets(searchParameters);
+            var theTweets2 = Search.SearchTweets(searchParameters);
+
             return searchKeyword + " in text.";
         }
 
