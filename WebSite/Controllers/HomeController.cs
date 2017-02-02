75429c2b42c2811084cbaadde74b67a4db95dc74
Dimitris <kaldim11@gmail.com>
2017-02-03 00:58:38 +0200

    Update HomeViewModel to hold-display info about messages

--- WebSite/Controllers/HomeController.cs
+++ WebSite/Controllers/HomeController.cs
@@ -9,16 +9,24 @@ using System.Web.Configuration;
 
 namespace WebSite.Controllers {
     public class HomeController: Controller {
+
         private FESearchRequestsController SReqController = new FESearchRequestsController();
         private HttpClient clientBEserver = new HttpClient {
             BaseAddress = new System.Uri(WebConfigurationManager.AppSettings["WebApiProviderURI"])
         };
 
+
         [HttpGet]
         public ActionResult Index() {
-            var searchRequests = SReqController.GetFESearchRequests();
+            HomeViewModel homeVM = new HomeViewModel();
+            homeVM.TheSearchRequests = SReqController.GetFESearchRequests();
+            if(TempData["bannerMsgCode"] == null) {
+                homeVM.TheBannerCode = BannnerMsgCode.None;
+            } else {
+                homeVM.TheBannerCode = (BannnerMsgCode)TempData["bannerMsgCode"];
+            }
 
-            return View(searchRequests);
+            return View(homeVM);
 
         }
 
@@ -34,59 +42,46 @@ namespace WebSite.Controllers {
             if(ModelState.IsValid) {
                 newSearchRequest.TheStatus = Status.Pending;
                 var response = await SReqController.PostFESearchRequest(newSearchRequest);
-
                 if(response.GetType() == typeof(CreatedAtRouteNegotiatedContentResult<FESearchRequest>)) {
                     FESearchRequest createdSearchRequest = ((CreatedAtRouteNegotiatedContentResult<FESearchRequest>)response).Content;
                     return await ExecuteSearchRequest(createdSearchRequest);
                 } else {
-                    //Error: SearchRequest could not be created
-                    return RedirectToAction("Index");
+                    return RedirectToIndex(BannnerMsgCode.ErrorNotCreated);
                 }
 
             } else {
-                //Error: Error while creating SearchRequest
-                return RedirectToAction("Index");
+                return RedirectToIndex(BannnerMsgCode.ErrorNotCreated);
             }
         }
 
         public async Task<ActionResult> ExecuteSearchRequest(int searchRequestID) {
-            var response = await SReqController.GetFESearchRequest(searchRequestID);
 
+            var response = await SReqController.GetFESearchRequest(searchRequestID);
             if(response.GetType() == typeof(OkNegotiatedContentResult<FESearchRequest>)) {
                 FESearchRequest searchRequest = ((OkNegotiatedContentResult<FESearchRequest>)response).Content;
                 return await ExecuteSearchRequest(searchRequest);
             } else {
-                await SReqController.UpdateSearchRequestStatus(searchRequestID, Status.Open);
-                //bale to swsto error (save sta drafts)
-                return RedirectToAction("Index");
+                await SReqController.UpdateSearchRequestStatus(searchRequestID, Status.Open);              
+                return RedirectToIndex(BannnerMsgCode.ErrorNotExecuted);
             }
 
         }
 
         private async Task<ActionResult> ExecuteSearchRequest(FESearchRequest searchRequest) {
+
             var response = await clientBEserver.PostAsJsonAsync("api/Service", (BaseSearchRequest)searchRequest);
             if(response.IsSuccessStatusCode) {
-                //All ok
+                return RedirectToIndex(BannnerMsgCode.CreateOk);
             } else {
                 await SReqController.UpdateSearchRequestStatus(searchRequest.ID, Status.Open);
-                //Error: SearchRequest could not be send for execution. Saved in drafts
+                return RedirectToIndex(BannnerMsgCode.ErrorNotExecuted);
             }
-
-            return RedirectToAction("Index");
         }
 
-        /*
-        public async Task<ActionResult> EndSearchRequest(int searchRequestID) {           
-            SearchRequest searchRequest = client.GetAsync("api/SearchRequests/" + searchRequestID).Result.Content.ReadAsAsync<SearchRequest>().Result;
-
-            int returnedTweets = ServiceController.getTweets(searchRequest.TheSearchKeyword, searchRequestID);
-
-            if(returnedTweets >= 0) {
-                await SReqController.UpdateSearchRequestStatus(searchRequestID, Status.Fulfilled);
-            }
+        System.Web.Mvc.RedirectToRouteResult RedirectToIndex(BannnerMsgCode bannerMsgCode) {
+            TempData["bannerMsgCode"] = bannerMsgCode;
             return RedirectToAction("Index");
         }
-        */
 
     }
 }
---
+++ WebSite/Models/HomeViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace WebSite.Models {
+
+    public enum BannnerMsgCode: int {
+        None,
+        CreateOk,
+        ErrorNotCreated,
+        ErrorNotExecuted
+    };
+
+    public class HomeViewModel {
+        public IEnumerable<FESearchRequest> TheSearchRequests { get; set; }
+        public BannnerMsgCode TheBannerCode { get; set; }
+
+        public static string[] BannerMsgText = {
+            "",
+            "Your Search Request was successfully created",
+            "Unfortunately, your Search Request could not be created. Please try again later.",
+            "Unfortunately, your Search Request could not be be programmed for execution right now, and was saved to your Drafts. Please try again later."
+        };
+
+        public static string[] AlertType = {
+            "",
+            "alert-success",
+            "alert-danger",
+            "alert-danger"
+        };
+
+        public string showBannerMsgText() {
+            return BannerMsgText[(int)(this.TheBannerCode)];
+        }
+
+        public string showBannerMsgAlert() {
+            return AlertType[(int)(this.TheBannerCode)];
+        }
+    }
+}
